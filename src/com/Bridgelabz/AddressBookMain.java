25194b74be13a6160b6eabbf41cfe40170cf3535
Divakar <divakar39312@gmail.com>
2022-08-16 09:42:18 +0530

    [Divakar]:createnew contact added

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -1,14 +1,49 @@
 package com.Bridgelabz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain {
+	static ArrayList<ContactPeople> personsList = new ArrayList<>();
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Address Book Program");
-		System.out.println("*******************************");
-		ContactPeople people = new ContactPeople();
-		people.contactDetails();
-		people.show();
+		AddressBookMain book = new AddressBookMain();
+		book.addContact();
+		
+	}
+	public void addContact(){
 
+		System.out.println("Plase Enter Number of Persons Want To Add in Address Book");
+		int numOfPersons = sc.nextInt();
+		for (int i=0;i<numOfPersons;i++) {
+			ContactPeople person1 = new ContactPeople();
+				
+			System.out.println("Enter first name:");
+			person1.setFirstName(sc.next());
+
+			System.out.println("Enter last name:");
+			person1.setLastName(sc.next());
+
+			System.out.println("Enter Address:");
+			person1.setAddress(sc.next());
+
+			System.out.println("Enter City name:");
+			person1.setCity(sc.next());
+
+			System.out.println("Enter state name:");
+			person1.setState(sc.next());
+
+			System.out.println("Enter zip code:");
+			person1.setZipNo(sc.nextInt());
+
+			System.out.println("Enter a phone number:");
+			person1.setPhoneNo(sc.nextLong());
+			personsList.add(person1);
+		}
+			System.out.print(personsList);
+			
 	}
 }
--- src/com/Bridgelabz/ContactPeople.java
+++ src/com/Bridgelabz/ContactPeople.java
@@ -4,42 +4,74 @@ import java.util.Scanner;
 
 public class ContactPeople {
 
-	String firstName,lastName,address,city,state;
+	private String firstName,lastName,address,city,state;
+	private int zipNo;
+	private long phoneNo;
 	
-     int zipNo;
-	long phoneNo;
-	
-	static Scanner sc = new Scanner(System.in);
-	public void contactDetails() {
-		System.out.println("Enter firstName");
-		firstName =sc.next();
-		
-		System.out.println("Enter LastName");
-		lastName =sc.next();
-		
-		System.out.println("Enter Address");
-		address =sc.next();
-		
-		System.out.println("Enter City");
-		city =sc.next();
-		
-		System.out.println("Enter State");
-		state =sc.next();
-		
-		System.out.println("Enter Zip");
-		zipNo =sc.nextInt();
-		
-		System.out.println("Enter phoneNumber");
-		phoneNo =sc.nextLong();
+	public ContactPeople(String firstName, String lastName, String address, String city, String state, int zipNo,
+			long phoneNo) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipNo = zipNo;
+		this.phoneNo = phoneNo;
 	}
 	
-	public void show() {
-		System.out.println( "\nPeople Details\n" );
-		System.out.println("FirstName= "+ firstName);
-		System.out.println("Address = "+ address);
-		System.out.println("city    = "+ city);
-		System.out.println("state  = "+ state);
-		System.out.println("zip  = " +zipNo);
-		System.out.println("phnNumber = " + phoneNo);
+	public ContactPeople() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZipNo() {
+		return zipNo;
+	}
+	public void setZipNo(int zipNo) {
+		this.zipNo = zipNo;
+	}
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	@Override
+	public String toString() {
+		return "\n PErson DETAILS \n firstName = " + firstName + " \n lastName = " + lastName 
+				+ "\n address = " + address + "\n city = "
+				+ city + "\n state = " + state + "\n zipNo = " + zipNo 
+				+ "\n phoneNo = " + phoneNo+"\n-Successfully added "+firstName+" into new contacts\n";
 	}
 }
